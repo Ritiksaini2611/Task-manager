b4dbfcf89f3db4af042d14c8c7fb4c015c0389b7
Ritik <saininishu093@gmail.com>
2022-03-21 22:46:00 +0530

    new task adding functionality

--- app.js
+++ app.js
@@ -14,3 +14,42 @@ function addTask() {
       alert("Please enter a task");
    }
 }
+
+// adding task to local storage
+
+function addTaskToLS() {
+   let tasks;
+   if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+   } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
+   }
+
+   tasks.push(input.value);
+   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+   console.log(input.value);
+   input.value = "";
+}
+
+function DisplayTask() {
+   let tasks;
+   if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+   } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
+   }
+
+   tasks.forEach(function (task, index) {
+      const newLi = document.createElement("li");
+      const complete = document.createElement("p");
+      const checkBtn = document.createElement("button");
+      const delBtn = document.createElement("button");
+
+      // adding icons to the button
+
+      checkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+      delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+      complete.textContent = "completed";
+   });
+}
